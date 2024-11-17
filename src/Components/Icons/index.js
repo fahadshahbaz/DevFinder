b95cffb5b431bd1f5428712d0a17981b90f8da98
Fahad Shahbaz <fahadshahbaz166@gmail.com>
2024-11-18 00:08:14 +0500

    Svg icons separated as component

---
+++ src/Components/Icons/index.js
@@ -0,0 +1,6 @@
+export { default as SunIcon } from './SunIcon';
+export { default as MoonIcon } from './MoonIcon';
+export { default as LocationIcon } from './LocationIcon'
+export { default as CompanyIcon } from './CompanyIcon';
+export { default as WebsiteIcon } from './WebisteIcon';
+// export { default as TwitterIcon } from './TwitterIcon';
